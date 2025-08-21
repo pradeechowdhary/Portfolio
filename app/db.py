df9cd8938d42e48189c08d9ae7b261c55ce24bf9
pradeechowdhary <123170341+pradeechowdhary@users.noreply.github.com>
2025-08-21 16:31:31 -0500

    Update db.py

--- app/db.py
+++ app/db.py
@@ -1,7 +1,7 @@
 import os
 from sqlmodel import SQLModel, create_engine, Session
 
-DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:///./chat.db")
+DATABASE_URL = os.getenv("DATABASE_URL", "postgresql://pponnam_user:4qL8N8KfV5FFCY8xkrzA0vqVwI83PsAp@dpg-d2jotmf5r7bs73e0fhrg-a/pponnam")
 
 # For SQLite in containers, disable same-thread check
 connect_args = {"check_same_thread": False} if DATABASE_URL.startswith("sqlite") else {}
